cd8ee85cc99595b9bf2713cc866786b8c777bd9e
auliafaizahr <zahaulia@gmail.com>
2021-02-10 18:28:54 +0700

    Config wickedpdf to bin

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,9 @@
 WickedPdf.config = {
-  exe_path: '/home/auliafaizahr/.rvm/gems/ruby-2.6.3/bin/wkhtmltopdf',
+  if Rails.env.production?
+    exe_path: "#{Rails.root}/bin/wkhtmltopdf"
+  else
+    exe_path = '/home/auliafaizahr/.rvm/gems/ruby-2.6.3/bin/wkhtmltopdf',
+  end
   enable_local_file_access: true,
   page_height: 110,
   page_width: 110,
